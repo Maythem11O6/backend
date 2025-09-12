8b3867da4639d57b8ebd397ce0f7058d8d92c112
Maythem11O6 <wolf2.0game@gmail.com>
2025-09-12 15:18:16 -0300

    back end formulario agregar productos
    agregar productos coon formulario html

--- bienvenida/inventario/models.py
+++ bienvenida/inventario/models.py
@@ -4,4 +4,4 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.CharField(max_length=1000)
     precio = models.DecimalField(decimal_places=2, max_digits=9999)
-    stock =models.IntegerField(default=0)
+    stock = models.IntegerField(default=0)
--- bienvenida/inventario/views.py
+++ bienvenida/inventario/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Producto
 from .forms import ProductoForm
 
+
 def producto_list(request):
     productos = Producto.objects.all()
-    return render(request, 'inventario/producto_list.html',
-    {'object_list': productos})
+    return render(request, 'inventario/producto_list.html', {'object_list': productos})
+
 
 def producto_detail(request, pk):
-    producto = get_object_or_404(Producto, pk=pk)
-    return render(request, 'inventario/producto_detail.html',
-    {'object': producto})
+    producto = get_object_or_404(klass=Producto, pk=pk)
+    return render(request, 'inventario/producto_detail.html', {'object': producto})
+
 
 def producto_create(request):
     if request.method == 'POST':
@@ -21,7 +22,8 @@ def producto_create(request):
     else:
         form = ProductoForm()
         return render(request, 'inventario/producto_form.html', {'form': form})
-    
+
+
 def producto_update(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
     if request.method == 'POST':
@@ -32,14 +34,11 @@ def producto_update(request, pk):
     else:
         form = ProductoForm(instance=producto)
         return render(request, 'inventario/producto_form.html', {'form': form})
-    
+
+
 def producto_delete(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
     if request.method == 'POST':
         producto.delete()
         return redirect('producto_list')
-    return render(request,
-'inventario/producto_confirm_delete.html', {'object': producto})
-
-
-
+    return render(request, 'inventario/producto_confirm_delete.html', {'object': producto})
